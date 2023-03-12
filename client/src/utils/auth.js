bfb42a589d8189dd7d6828ae26f1997d90b917d9
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-03-12 11:25:13 -0500

    connecting front and back to render pages, homepage-trainees working

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,6 +1,6 @@
 // import { useNavigate } from 'react-router-dom';
 // use this to decode a token and get the user's information out of it
-import decode from 'jwt-decode';
+import decode from "jwt-decode";
 
 // create a new class to instantiate for a user
 class AuthService {
@@ -17,32 +17,34 @@ class AuthService {
 
   getToken() {
     // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
+    return localStorage.getItem("id_token");
   }
-  
+
   isTokenExpired(token) {
     // Decode the token to get its expiration time that was set by the server
     const decoded = decode(token);
     // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
     if (decoded.exp < Date.now() / 1000) {
-      localStorage.removeItem('id_token');
+      localStorage.removeItem("id_token");
       return true;
     }
     // If token hasn't passed its expiration time, return `false`
     return false;
   }
 
-  login(idToken) {
+  login(data) {
+    console.log(data);
     // Saves user token to localStorage and reloads the application for logged in status to take effect
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
-    
+    localStorage.setItem("id_token", data.login.token);
+    localStorage.setItem("trainer_id", data.login.user._id);
+    window.location.assign("/");
   }
 
   logout() {
     // const navigate = useNavigate();
     // // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("trainer_id");
     // this will reload the page and reset the state of the application
     window.location.reload();
     // navigate('/login');

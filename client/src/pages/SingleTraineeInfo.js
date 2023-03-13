e13287c9da45a65e732bd5916cfaf37a88258156
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-03-13 16:26:38 -0500

    updated singletrineepage

--- client/src/pages/SingleTraineeInfo.js
+++ client/src/pages/SingleTraineeInfo.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-function SingleTraineeInfo({trainee}) {
+function SingleTraineeInfo({trainee},{traineeDemo}) {
     console.log(trainee, "====")
     return (
         <div>
            <p className="card-text"><b>First Name:</b> {trainee.firstName}</p>
             <p className="card-text"><b>Last Name:</b> {trainee.lastName}</p>
-            <p className="card-text"><b>Weight:</b> {trainee.demographics.weight}</p>
-            <p className="card-text"><b>Height:</b> {trainee.demographics.height}</p>
+            <p className="card-text"><b>Weight:</b> {trainee.demographics[0].weight}</p>
+            <p className="card-text"><b>Height:</b> {trainee.demographics[0].height}</p>
             <p className="card-text"><b>DOB:</b> {trainee.dob}</p>
-                <p className="card-text"><b>Goals:</b> {trainee.demographics.goals}</p>
-                <p className="card-text"><b>Injury History:</b> {trainee.demographics.injuryHistory}</p>
-                <p className="card-text"><b>Notes:</b> {trainee.demographics.notes}</p>
+                <p className="card-text"><b>Goals:</b> {trainee.demographics[0].goals}</p>
+                <p className="card-text"><b>Injury History:</b> {trainee.demographics[0].injuryHistory}</p>
+                <p className="card-text"><b>Notes:</b> {trainee.demographics[0].notes}</p>
         </div>
     )
 };

0bdb3fb3aa0923567046847344039c2cb29150ce
Erin <erinpeifer@gmail.com>
2023-03-13 00:13:03 -0500

    edit workout Plan dropdowns work

--- client/src/components/EditWorkoutPlanModal/EditWorkoutPlanModal.js
+++ client/src/components/EditWorkoutPlanModal/EditWorkoutPlanModal.js
@@ -4,37 +4,47 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import {QUERY_EXERCISES} from '../../utils/queries'
 
-
 function EditWorkoutPlanModal(props) {
   const [show, setShow] = useState(false);
   const {loading, data } = useQuery(QUERY_EXERCISES);
-
   const exerciseList = data?.exercises || [];
-  // console.log(exerciseList);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const muscleGroups = []
+
   exerciseList.map((exercise) => {
     return (muscleGroups.push(exercise.muscleGroup))
   });
 
   const isolatedGroups = [...new Set(muscleGroups)];
 
-  const [selectedGroup, setSelectedGroup] = useState("Arms");
-  console.log("line22", selectedGroup)
-  const handleInputChange = (e) => {
+
+  const [selectedGroup, setSelectedGroup] = useState("");
+  const handleInputChange =  (e) => {
   const { name, value } = e.target;
-  setSelectedGroup({ [name]: value })
-  // console.log(selectedGroup);        
+  setSelectedGroup({ [name]: value });
+  optionsArr = [];
+  populateExercises(selectedGroup);
 }
+console.log("25", selectedGroup.musclegroup)
 const exercisesArr = [];
-  // .then((selectedGroup) => {
-   populateExercises(selectedGroup.musclegroup)
-   async function populateExercises (){
-console.log("line 32", selectedGroup.musclegroup)
+const optionsArr = [];
+exerciseList.map((exercise) => {
+  return (exercisesArr.push(exercise)
+    );
+});
+
+populateExercises(selectedGroup)
+
+
+  console.log("36", selectedGroup.musclegroup);  
+function populateExercises (selectedGroup) {
+    console.log("38", selectedGroup)
       switch (selectedGroup.musclegroup) {
       case 'Arms':
         isArms();
         
-        console.log(selectedGroup.musclegroup)
+        console.log("43", selectedGroup)
         break;
       case 'Back':
         isBack();
@@ -55,59 +65,76 @@ console.log("line 32", selectedGroup.musclegroup)
       case 'Shoulders':
         isShoulders();
         console.log(selectedGroup.musclegroup)
-        break;    
-
+        break;
+      default:
+        // console.log(exercisesArr)
     }}
   // }
   // );
   
-// console.log(selectedGroup)
+console.log("70", selectedGroup.musclegroup)
 
-exerciseList.map((exercise) => {
-  return (exercisesArr.push(exercise)
-    );
-});
+async function isArms() {
+  console.log("isArms", selectedGroup.musclegroup)
+  const arms = exercisesArr.filter(function (a) {
+    return a.muscleGroup === "Arms";});
+    arms.map((options) => {
+      optionsArr.push(options.exerciseName)
+    })
+    ;
+  console.log(optionsArr);
+}
 
-async function isArms(options) {
-  const arms = await exercisesArr.filter(function (el) {
-  return (el.muscleGroup === "Arms")
-  // .then ( arms.map(options));
-})}
+async function isBack() {
+  console.log("isBack", selectedGroup)
+  const back = exercisesArr.filter(function (b) {
+    return b.muscleGroup === "Back";});
+    back.map((options) => {
+      optionsArr.push(options.exerciseName)
+    })
+  console.log(back);
+};
 
-async function isBack(options) { 
-  const back = exercisesArr.filter(function (el) {
-  return (el.muscleGroup === "Back").then(
-  console.log(back.map()))
-  // .then ( back.map(options));
-})};
+async function isChest() {
+  console.log("isChest", selectedGroup)
+  const chest = exercisesArr.filter(function (c) {
+    return c.muscleGroup === "Chest";});
+    chest.map((options) => {
+      optionsArr.push(options.exerciseName)
+    })
+  console.log(chest);
+};
 
-async function isChest(options) {
-  const chest = await exercisesArr.filter(function (el) {
-  return (el.muscleGroup === "Chest")
-  // .then ( chest.map(options))
-})};
+async function isGlutes() {
+  console.log("isGlutes", selectedGroup)
+  const glutes = exercisesArr.filter(function (g) {
+    return g.muscleGroup === "Glutes";});
+    glutes.map((options) => {
+      optionsArr.push(options.exerciseName)
+    })
+  console.log(glutes);
+};
 
-async function isGlutes(options) {
-  const glutes = await exercisesArr.filter(function (el) {
-  return (el.muscleGroup === "Glutes")
-  // .then ( glutes.map(options));
-})};
+async function isLegs() {
+  console.log("isLegs", selectedGroup)
+  const legs = exercisesArr.filter(function (l) {
+    return l.muscleGroup === "Legs";})
+    legs.map((options) => {
+      optionsArr.push(options.exerciseName)
+    })
+  console.log(legs);
+};
 
-async function isLegs(options) {
-  const legs = await exercisesArr.filter(function (el) {
-  return (el.muscleGroup === "Legs")
-  // .then ( legs.map(options));
-})};
+async function isShoulders() {
+  console.log("isShoulders", selectedGroup)
+  const shoulders = exercisesArr.filter(function (s) {
+    return s.muscleGroup === "Shoulders";});
+    shoulders.map((options) => {
+      optionsArr.push(options.exerciseName)
+    })
+  console.log(shoulders);
+};
 
-async function isShoulders(options) {
-  const shoulders = await exercisesArr.filter(function (el) {
-  return (el.muscleGroup === "Shoulders")
-  // .then ( shoulders.map(options));
-})};
-
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
@@ -135,50 +162,47 @@ async function isShoulders(options) {
          <form className="row justify-content-center" >
          <div className="dropdown">
          <select name="musclegroup" onChange={handleInputChange}>
+          <option>Select Muscle Group</option>
               {isolatedGroups.map((group) => {
                 return (
+                  
                   <option key={group} value={group}>
                     {group}
                   </option>
                 );
               })}
             </select>
-            {/* <label>Exercise: </label>
+            {/* <label>Exercise: </label> */}
             <select name="exerciseName"
             //  onChange={handleInputChange}
              >
-              {populateExercises((options) => {
+              <option>Select Exercise</option>
+              {optionsArr.map((option) => {
                 return (
-                  <option key={options} value={options}>
-                    {options}
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                );
+              })}
+            </select>
+            {/* THIS CODE WORKS FOR ALL EXERCISES
+            <select name="exerciseName"
+            //  onChange={handleInputChange}
+             >
+              {exerciseList.map((option) => {
+                return (
+                  <option key={option} value={option.exerciseName}>
+                    {option.exerciseName}
                   </option>
                 );
               })}
             </select> */}
-          {/* <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Muscle Group
-          </button>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="">Action</a></li>
-            <li><a className="dropdown-item" href="">Another action</a></li>
-            <li><a className="dropdown-item" href="">Something else here</a></li>
-          </ul>
-        </div>
-        <div className="dropdown">
-          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Exercise
-          </button>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="">Action</a></li>
-            <li><a className="dropdown-item" href="">Another action</a></li>
-            <li><a className="dropdown-item" href="">Something else here</a></li>
-          </ul>*/}
         </div> 
             <p className=''>Sets:
             <input 
             // value={sets} 
             name="sets" 
-            onChange={handleInputChange} 
+            // onChange={handleInputChange} 
             type="number"
             placeholder="Sets" 
             className='inputStyle'
@@ -187,7 +211,8 @@ async function isShoulders(options) {
             <input 
             // value={reps} 
             name="reps" 
-            onChange={handleInputChange} type="number" 
+            // onChange={handleInputChange}
+            type="number" 
             placeholder="reps" 
             className='inputStyle'
             /></p>
@@ -195,7 +220,7 @@ async function isShoulders(options) {
             <input 
             // value={weight} 
             name="weight" 
-            onChange={handleInputChange} 
+            // onChange={handleInputChange} 
             type="Number" 
             placeholder="lbs" 
             className='inputStyle'
@@ -204,7 +229,7 @@ async function isShoulders(options) {
             <input 
             // value={distance} 
             name="distance" 
-            onChange={handleInputChange} 
+            // onChange={handleInputChange} 
             type="number" 
             placeholder="Distance" 
             className='inputStyle'
@@ -213,7 +238,7 @@ async function isShoulders(options) {
             <input 
             // value={equipmentReq} 
             name="equipmentReq" 
-            onChange={handleInputChange} 
+            // onChange={handleInputChange} 
             type="text"
             placeholder="Equipment Required" 
             className='inputStyle'
@@ -222,7 +247,7 @@ async function isShoulders(options) {
             <input 
             // value={notes} 
             name="notes" 
-            onChange={handleInputChange} 
+            // onChange={handleInputChange} 
             type="text"
             placeholder="Notes: (instructions, focal points, exceptions, etc.)" 
             className='inputStyle'

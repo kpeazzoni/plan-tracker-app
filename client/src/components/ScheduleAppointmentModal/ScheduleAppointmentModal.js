478611f6f66f94e1d1714c87494b7fb849e5ede1
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-03-14 22:39:37 -0400

    Revert "saved the work I have been doing on the Schedule appointment js."

--- client/src/components/ScheduleAppointmentModal/ScheduleAppointmentModal.js
+++ client/src/components/ScheduleAppointmentModal/ScheduleAppointmentModal.js
@@ -1,77 +1,30 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { ADD_APPOINTMENT } from '../../utils/mutations';
-import { useMutation } from '@apollo/client';
 
 
 function ScheduleAppointmentModal(props) {
-  const [formState, setFormState] = useState({
-    
-    startDate: '',
-    endDate: '',
-    location: '',
-  });
-
-  const [addAppointment] = useMutation(ADD_APPOINTMENT);
-
   const [show, setShow] = useState(false);
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
   const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
     const { name, value } = e.target;
-    console.log('name', name, 'value', value)
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+  }
 
-  const handleDateFormat = () => {
-    const formattedTimeStampStart = `${formState}T${formState.startDate}:00`;
-    const formattedTimeStampEnd = `${formState}T${formState.endDate}:00`;
-  
-    const formattedStartDateTime = (formattedTimeStampStart, "isoDateTime");
-    const formattedEndDateTime = (formattedTimeStampEnd, "isoDateTime");
-  
-    return { 
-      startDate: formattedStartDateTime, 
-      endDate: formattedEndDateTime 
-    };
-  };
-  
-  
-  const handleFormSubmit = async (e) => {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(formState);
-    try {
-      const { startDate, endDate } = handleDateFormat(); // destructure start and end date
-      const { data } = await addAppointment({
-        variables: { 
-          ...formState,
-          startDate, // use destructured values directly
-          endDate
-        },
-      });
-  
-      console.log('data', data);
-      // Reset the form state to clear the input data
-      setFormState({
-        startDate: '',
-        endDate: '',
-        location: '',
-      });
-    } catch (e) {
-      console.error(e);
-    }
   };
-  
+  // date: '',
+  // startTime: '',
+  // endTime: '',
+  // location: '',
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        ScheduleAppointmentModal
+      <Button className = 'btn onWhite' onClick={handleShow}>
+        Schedule Appointment
       </Button>
 
       <Modal show={show} onHide={handleClose}>
@@ -81,63 +34,118 @@ function ScheduleAppointmentModal(props) {
         <Modal.Body>
           <div className='container col-8'>
             <form className="row justify-content-center formCard" >
-              <p className=''>Date:
+              <div className="input-group">
+                <span className="input-group-text" id="basic-addon3">Date</span>
                 <input
-                  value={formState.date}
-                  name="Date"
+                  name="date"
                   onChange={handleInputChange}
                   type="Date"
                   placeholder="Date"
-                  className='inputStyle'
-                /></p>
-         
-              <p className=''>Start Time:
-                <select className="form-control" onChange={handleInputChange} name ="startDate" id="exampleFormControlSelect1">
-                  <option value={`05:00:00`}>5:00am</option>
-                  <option value={`05:30:00`}>5:30am</option>
-                  <option value={`06:00:00`}>6:00am</option>
-                  <option value={`06:30:00`}>6:30am</option>
-                  <option value={`07:00:00`}>7:00am</option>
-                  <option value={`07:30:00`}>7:30am</option>
-                  <option value={`08:30:00`}>8:00am</option>
-                  <option value={`08:30:00`}>8:30am</option>
-                  <option value={`08:30:00`}>9:00am</option>
-                  <option value={`09:30:00`}>9:30am</option>
+                  className="form-control" />
+              </div>
+              <div className="input-group">
+                <span className="input-group-text">Time</span>
+                <select
+                  name="startTime"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="Start"
+                  className="form-control"
+                >
+                  <option>5:00am</option>
+                  <option>5:30am</option>
+                  <option>6:00am</option>
+                  <option>6:30am</option>
+                  <option>7:00am</option>
+                  <option>7:30am</option>
+                  <option>8:00am</option>
+                  <option>8:30am</option>
+                  <option>9:00am</option>
+                  <option>9:30am</option>
+                  <option>10:00am</option>
+                  <option>10:30am</option>
+                  <option>11:00am</option>
+                  <option>11:30am</option>
+                  <option>12:00pm</option>
+                  <option>12:30pm</option>
+                  <option>1:00pm</option>
+                  <option>1:30pm</option>
+                  <option>2:00pm</option>
+                  <option>2:30pm</option>
+                  <option>3:00pm</option>
+                  <option>3:30pm</option>
+                  <option>4:00pm</option>
+                  <option>4:30pm</option>
+                  <option>5:00pm</option>
+                  <option>5:30pm</option>
+                  <option>6:00pm</option>
+                  <option>6:30pm</option>
+                  <option>7:00pm</option>
+                  <option>7:30pm</option>
+                  <option>8:00pm</option>
+                  <option>8:30pm</option>
+                  <option>9:00pm</option>
+                  <option>9:30pm</option>
                 </select>
-              </p>
-              <p className=''>End Time:
-                <select className="form-control" onChange={handleInputChange} name ="endDate" id="exampleFormControlSelect1">
-                  <option value={`05:30:00`}>5:00am</option>
-                  <option value={`05:30:00`}>5:30am</option>
-                  <option value={`06:00:00`}>6:00am</option>
-                  <option value={`06:30:00`}>6:30am</option>
-                  <option value={`07:30:00`}>7:00am</option>
-                  <option value={`07:30:00`}>7:30am</option>
-                  <option value={`08:30:00`}>8:00am</option>
-                  <option value={`08:30:00`}>8:30am</option>
-                  <option value={`09:30:00`}>9:00am</option>
-                  <option value={`09:30:00`}>9:30am</option>
-                  <option value={`10:30:00`}>10:00am</option>
-                  <option value={`10:30:00`}>10:30am</option>
+                <select
+                  name="EndTime"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="End"
+                  className="form-control"
+                >
+                  <option>5:00am</option>
+                  <option>5:30am</option>
+                  <option>6:00am</option>
+                  <option>6:30am</option>
+                  <option>7:00am</option>
+                  <option>7:30am</option>
+                  <option>8:00am</option>
+                  <option>8:30am</option>
+                  <option>9:00am</option>
+                  <option>9:30am</option>
+                  <option>10:00am</option>
+                  <option>10:30am</option>
+                  <option>11:00am</option>
+                  <option>11:30am</option>
+                  <option>12:00pm</option>
+                  <option>12:30pm</option>
+                  <option>1:00pm</option>
+                  <option>1:30pm</option>
+                  <option>2:00pm</option>
+                  <option>2:30pm</option>
+                  <option>3:00pm</option>
+                  <option>3:30pm</option>
+                  <option>4:00pm</option>
+                  <option>4:30pm</option>
+                  <option>5:00pm</option>
+                  <option>5:30pm</option>
+                  <option>6:00pm</option>
+                  <option>6:30pm</option>
+                  <option>7:00pm</option>
+                  <option>7:30pm</option>
+                  <option>8:00pm</option>
+                  <option>8:30pm</option>
+                  <option>9:00pm</option>
+                  <option>9:30pm</option>
                 </select>
-              </p>
-              <p className=''>Location:
+              </div>
+              <div className="input-group">
+                <span className="input-group-text" id="basic-addon3">Location</span>
                 <input
-                  // value={location} 
                   name="location"
                   onChange={handleInputChange}
                   type="text"
-                  placeholder="Location"
-                  className='inputStyle'
-                /></p>
+                  className="form-control" />
+              </div>
             </form>
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button className= "btn onWhite" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleFormSubmit}>
+          <Button className = "btn onWhite" onClick={handleFormSubmit}>
             Save Changes
           </Button>
         </Modal.Footer>

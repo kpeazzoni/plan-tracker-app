7ee41d597cd416085fdcc3ea561963991d3d033c
mjtic <myeonghun.jeong88@gmail.com>
2023-09-29 10:28:08 -0500

    changing DB

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/plantrackerDB',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/plantracker_DB',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
